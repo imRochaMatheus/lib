6119f98bbd3e146d603d46250ff6ba2034194c47
imrochamatheus <im.rochamatheus@gmail.com>
2021-05-20 21:08:31 -0300

    Atualizando migrations

---
+++ database/migrations/2021_04_27_004745_create_emprestimos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmprestimosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emprestimos', function (Blueprint $table) 
+        {
+            $table->id();
+            $table->unsignedBigInteger('id_estudante');
+            $table->unsignedBigInteger('id_funcionario');
+            $table->datetime('data_emprestimo');
+            $table->double('multa')->default(0);
+
+            $table->foreign('id_estudante')->references('id')->on('estudantes');
+            $table->foreign('id_funcionario')->references('id')->on('funcionarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('emprestimos_id_estudante_foreign');
+        $table->dropForeign('emprestimos_id_funcionario_foreign');
+        
+        Schema::dropIfExists('emprestimos');
+    }
+}
---
+++ database/migrations/2021_04_28_235653_create_emprestimo_contem_exemplar_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmprestimoContemExemplarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emprestimo_contem_exemplar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('emprestimo_id');
+            $table->unsignedBigInteger('codigo_exemplar');
+            $table->date('data_limite');
+            $table->date('data_devolucao');
+            $table->integer('renovacoes')->default(3);
+            $table->boolean('status')->default(false);
+
+            $table->foreign('codigo_exemplar')->references('codigo')->on('exemplares');
+            $table->foreign('emprestimo_id')->references('id')->on('emprestimos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('restimo_contem_exemplar_emprestimo_id_foreign');
+        $table->dropForeign('restimo_contem_exemplar_exemplar_id_foreign');
+        
+        Schema::dropIfExists('emprestimo_contem_exemplar');
+    }
+}
